1ab8972ef2d7212835cb817ad045ff0fc3a5b18e
Isaac Cambron <isaac@isaaccambron.com>
2017-04-07 10:01:24 -0400

    even better wrapping

--- env/dev/cljs/reagent_slider/dev.cljs
+++ env/dev/cljs/reagent_slider/dev.cljs
@@ -14,31 +14,35 @@
                   5 "6 mos"
                   6 "1 year"})
 
-(defn wrapper-component [model cback]
+(defn wrapper-component [internal-value external-value opts]
   [core/tooltipped-range-component
-   {:min 0
-    :max (dec (count index->time))
-    :value [(:min @model) (:max @model)]
-    :marks index->time
-    :onAfterChange (fn [_] (cback))
-    :onChange (fn [[v1 v2]]
-                (reset! model {:min (min v1 v2)
-                               :max (max v1 v2)}))
-    :tipFormatter #(index->time %)}])
+   (merge
+    {:value [(:min @internal-value) (:max @internal-value)]
+     :onAfterChange (fn [_] (reset! external-value @internal-value))
+     :onChange (fn [[v1 v2]]
+                 (reset! internal-value {:min (min v1 v2)
+                                         :max (max v1 v2)}))}
+    opts)])
 
 (defn boot-demo-app []
 
   (let [model (reagent/atom {:min 2 :max 3})
-        active-model (reagent/atom @model)
-        update! #(reset! active-model @model)]
+        slider-model (reagent/atom @model)]
 
     (reagent/render [:div {:style {:width 290}}
                      [:button {:on-click #(do
-                                            (reset! model {:min 1 :max 5})
-                                            (update!))} "switch!"]
-                     [wrapper-component model update!]](.getElementById js/document "app"))
+                                            (reset! slider-model {:min 1 :max 5})
+                                            (reset! model @slider-model))} "switch!"]
+                     [wrapper-component
+                      slider-model
+                      model
+                      {:min 0
+                       :max (dec (count index->time))
+                       :marks index->time
+                       :tipFormatter #(index->time %)}]]
+                    (.getElementById js/document "app"))
 
-    (add-watch active-model :watcher (fn [_ _ _ _] (println @active-model)))))
+    (add-watch model :watcher (fn [_ _ _ _] (println @model)))))
 
 (figwheel/watch-and-reload
   :websocket-url "ws://localhost:3450/figwheel-ws"
